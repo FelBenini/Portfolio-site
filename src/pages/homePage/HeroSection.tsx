c92fa06452e0d88113d8a5509d719b6b5bacf359
FelBenini <felbenini@gmail.com>
2023-02-24 17:14:03 -0300

    Added hyperlinks to github and linkedin icons

--- src/pages/homePage/HeroSection.tsx
+++ src/pages/homePage/HeroSection.tsx
@@ -19,16 +19,20 @@ const HeroSection = () => {
                         }} />
                 </h3>
                 <span className='fade'>
+                    <a href='https://github.com/FelBenini' target='blank'>
                     <Tooltip title="GitHub">
                         <IconButton color='secondary'>
                             <img src='/img/github-icon.svg' alt='github profile' />
                         </IconButton>
                     </Tooltip>
+                    </a>
+                    <a href='https://www.linkedin.com/in/felipe-benini-88a23b24a/' target='blank'>
                     <Tooltip title="LinkedIn" style={{marginLeft: '10px'}}>
                         <IconButton color='secondary'>
                             <img src='/img/linkedin-icon.svg' alt='linkedin profile' />
                         </IconButton>
                     </Tooltip>
+                    </a>
                     <button style={{marginLeft: '10px'}} id='download-cv'>Download CV</button>
                 </span>
             </div>
