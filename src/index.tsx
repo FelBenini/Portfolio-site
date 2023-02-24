55a2c3aad81c793f987e50ed355697896bed41a3
FelBenini <felbenini@gmail.com>
2023-02-24 15:28:51 -0300

    Added github and linkedin link icons

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFFFFF',
+    },
+    secondary: {
+      main: '#08BF9E',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/homePage/HeroSection.tsx
+++ src/pages/homePage/HeroSection.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect'
+import { IconButton } from '@mui/material'
+import Tooltip from '@mui/material/Tooltip'
 
 const HeroSection = () => {
-  return (
-    <section id="heroSection">
-    <div id="heroSectionText">
-      <h5 className='fade'>Hey there, I am</h5>
-      <h1 className='fade'>Felipe Benini</h1>
-      <h3 className='fade'>
-      <Typewriter
-        options={{
-          strings: ['a Web Developer', 'a Full Stack Engineer', 'a Web Designer', 'a Full Stack Developer'],
-          autoStart: true,
-          loop: true,
-          delay: 350
-        }} />
-        </h3>
-    </div>
-    <img className='fade' src='/img/legoman-portfolio.webp' alt='Lego space astronaut'/>
-  </section>
-  )
+    return (
+        <section id="heroSection">
+            <div id="heroSectionText">
+                <h5 className='fade'>Hey there, I am</h5>
+                <h1 className='fade'>Felipe Benini</h1>
+                <h3 className='fade'>
+                    <Typewriter
+                        options={{
+                            strings: ['a Web Developer', 'a Full Stack Engineer', 'a Web Designer', 'a Full Stack Developer'],
+                            autoStart: true,
+                            loop: true,
+                            delay: 350
+                        }} />
+                </h3>
+                <span className='fade'>
+                    <Tooltip title="GitHub">
+                        <IconButton color='secondary'>
+                            <img src='/img/github-icon.svg' alt='github profile' />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="LinkedIn" style={{marginLeft: '10px'}}>
+                        <IconButton color='secondary'>
+                            <img src='/img/linkedin-icon.svg' alt='linkedin profile' />
+                        </IconButton>
+                    </Tooltip>
+                </span>
+            </div>
+            <img id='header-img' className='fade' src='/img/legoman-portfolio.webp' alt='Lego space astronaut' />
+        </section>
+    )
 }
 
 export default HeroSection
