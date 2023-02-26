1b66b41ae95945b7b4dd4400205d017146704b79
FelBenini <felbenini@gmail.com>
2023-02-26 17:02:47 -0300

    Updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header></header>
+    <header>
+        <Link id='logo' to='/'><img src='/img/logo.webp' alt='logo'/></Link>
+        <div>
+            <Link to='/about'><h5>About</h5></Link>
+            <h5>Contact</h5>
+        </div>
+    </header>
   )
 }
 
