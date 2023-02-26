911534377ad45cea58385f82b607f4a0d1fb1de1
FelBenini <felbenini@gmail.com>
2023-02-26 19:13:44 -0300

    Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './style/App.scss';
 import Homepage from './pages/homePage/Homepage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Aboutpage from './pages/aboutPage/Aboutpage';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="" element={<Homepage />} />
+        <Route path="" element={<Homepage/>} />
+        <Route path="/about" element={<Aboutpage/>} />
       </Routes>
     </BrowserRouter>
   );
