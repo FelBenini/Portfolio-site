1815adc7fc8d301583db66263422e478519a1090
FelBenini <felbenini@gmail.com>
2023-02-26 16:37:54 -0300

    Added Browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './style/App.css';
 import Homepage from './pages/homePage/Homepage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header></header>
-      <Homepage/>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
