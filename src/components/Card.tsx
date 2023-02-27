b74b1571e2d426c6ecdc95e9d5cb67e307e4fbe7
FelBenini <felbenini@gmail.com>
2023-02-27 16:05:29 -0300

    Updated the projects section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,9 @@ interface cardProps {
     name: string,
     img: string,
     description: string,
+    github: string,
+    livedemo: boolean,
+    livedemolink: string
 }
 
 const Card = (props: cardProps) => {
@@ -11,15 +14,17 @@ const Card = (props: cardProps) => {
         <div className="card">
             <div className="card-content">
                 <div className="card-image">
-                    <i className="fa-duotone fa-apartment"></i>
                 </div>
                 <div className="card-info-wrapper">
                     <div className="card-info">
-                        <i className="fa-duotone fa-apartment"></i>
                         <div className="card-info-title">
                             <h5>{props.name}</h5>
                             <img src={props.img} alt="Project"/>
                             <p>{props.description}</p>
+                            <span className='buttons'>
+                                <a href={props.github} target='blank'><button>Github</button></a>
+                                {props.livedemo === true ? <a href={props.livedemolink} target='blank'><button style={{color: '#08BF9E'}}>Live Demo</button></a> : <p style={{display: 'none'}}></p>}
+                            </span>
                         </div>
                     </div>
                 </div>
