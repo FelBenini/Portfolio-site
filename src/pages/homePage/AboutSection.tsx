de8c1392fcbeeed6d88922b55a1ddfec329d18d4
FelBenini <felbenini@gmail.com>
2023-02-26 21:51:13 -0300

    Changed about section text

--- src/pages/homePage/AboutSection.tsx
+++ src/pages/homePage/AboutSection.tsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
     <section id='aboutSection'>
       <h3><img src='/img/user-icon.svg' alt='User Icon'/>About me</h3>
       <div className='textPart'>
-        <p>Hello, my name is <em>Felipe Benini</em> and I am a Full Stack developer with skills in <em>programming languages</em> such as JavaScript, TypeScript, PHP, NodeJS, and <em>frameworks</em> such as React, jQuery, and Express. My expertise also includes working with <em>databases</em> such as MySQL and MongoDB, in addition to the creation and implementation of RESTful <em>APIs</em> and the integration of applications with external APIs.</p>
+        <p>Hello, my name is <em>Felipe Benini</em> and I am a Full Stack developer with skills in <em>programming languages</em> such as JavaScript, TypeScript, PHP, NodeJS, and <em>frameworks</em> and libraries such as React, jQuery, and Express. My expertise also includes working with <em>databases</em> such as MySQL and MongoDB, in addition to the creation and implementation of RESTful <em>APIs</em> and the integration of applications with external APIs.</p>
         <TagCloud
           options={(w: Window & typeof globalThis) => ({
             radius: Math.min(350, w.innerWidth, w.innerHeight) / 2,
