46ba5074e8dddb3c7c4ecad6a19dc0029785b886
FelBenini <felbenini@gmail.com>
2023-02-26 22:10:45 -0300

    Header modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,9 @@ function Header() {
     <header>
         <Link id='logo' to='/'><img src='/img/logo.webp' alt='logo'/></Link>
         <div>
+            <Link to='/projects'><h5>Projects</h5></Link>
             <Link to='/about'><h5>About</h5></Link>
-            <h5>Contact</h5>
+            <Link to='/contact'><h5>Contact</h5></Link>
         </div>
     </header>
   )
