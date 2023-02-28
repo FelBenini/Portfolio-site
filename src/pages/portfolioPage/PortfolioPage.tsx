f6d9ecd3ef2bf58540d444d65a942e41e1d2d6c6
FelBenini <felbenini@gmail.com>
2023-02-28 14:58:06 -0300

    Added portfolio website to projects

--- src/pages/portfolioPage/PortfolioPage.tsx
+++ src/pages/portfolioPage/PortfolioPage.tsx
@@ -16,11 +16,13 @@ const PortfolioPage = () => {
     return (
         <section id='portfolio-page'>
             <div id='cards' onMouseMove={cardAnimation}>
-                <Card name='CryptoTracker' img='/img/cryptotracker-img.webp' description="A crypto tracker web app made with React, CoinGecko's API and other libraries." github='https://github.com/FelBenini/crypto_tracker' livedemo={true} livedemolink='https://crypto-tracker-fb.vercel.app/' />
+                <Card name='CryptoTracker' img='/img/cryptotracker-img.webp' description="A crypto tracker web app made with React, CoinGecko's API and SASS." github='https://github.com/FelBenini/crypto_tracker' livedemo={true} livedemolink='https://crypto-tracker-fb.vercel.app/' />
 
                 <Card name='Todo App' img='/img/todo-app-img.webp' description='A task manager made with React, TypeScript, cookies and other libraries like Material UI.' github='https://github.com/FelBenini/todo-app' livedemo={true} livedemolink='https://todo-app-felbenini.vercel.app/' />
 
                 <Card name='QR Code Generator' img='https://felipe-benini.netlify.app/img/project_4.png' description='A QR Code generator made with HTML 5, CSS 3, JavaScript, the jQuery library and an API.' github='https://github.com/FelBenini/qr-code-generator' livedemo={true} livedemolink='https://liveqr-code-generator.netlify.app/' />
+
+                <Card name='This Website' img='/img/portfolio-site-img.webp' description='My personal website made with React, TypeScript and SASS to introduce myself and my portfolio.' github='https://github.com/FelBenini/Portfolio-site' livedemo={true} livedemolink='https://felbenini.vercel.app/' />
                 
                 <Card name='TypeScript Calculator' img='/img/typescript-calculator.webp' description='A fully functional TypeScript calculator, made so I could get started with the TypeScript syntax.' github='https://github.com/FelBenini/TypeScript-Calculator' livedemo={true} livedemolink='https://typescript-calculator-fb.netlify.app/' />
 
